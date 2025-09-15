a21d33dfa8f1892cb7c03a5f583f16ecc3499749
Pedro Alves Estevam Magaieski <46140459877@>
2025-09-15 11:21:20 -0300

    mapear, focar em filtrar e puxar usuario por nome

--- FrontendKanban/src/pages/Quadro.jsx
+++ FrontendKanban/src/pages/Quadro.jsx
@@ -1,33 +1,54 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export function Quadro(){
+    const [cartoes,setCartoes] = useState([]);
+
+        useEffect(() => {
+        // Definir uma funcao asincrona no useEffect
+        const buscarCartões = async () => {
+            try {
+                const res = await axios.get("http://127.0.0.1:8000/api/criarTarefa/");
+                setCartoes(res.data);
+            } catch (err) {
+                console.error("Erro ao buscar Cartões: ", err);
+            }
+        };
+        // Chamar a funcao
+        buscarCartões();
+    }, []);
+
     return(
         <main>
             <h1>Kanban: </h1>
             <section className="quadro">
                 <section className="coluna">
                     <h2>A fazer:</h2>
+                    {cartoes.map((c)=> (
+                        <section className="cartao">
+                            <ul className="tarefa">
+                                <section>
+                                    <li>Descrição: {c.descricao}</li>
+                                    <li>Setor: {c.nomeSetor}</li>
+                                    <li>Prioridade: {c.prioridade}</li>
+                                    <li>Vinculado a: {c.usuario}</li>
+                                </section>
+                                <section>
+                                    <button className="tarefa">editar</button>
+                                    <button className="tarefa">excluir</button>
+                                </section>
+                                <section>
+                                    <select name="nome" id="id">
+                                        <option value="">A fazer</option>
+                                        <option value="">Fazendo</option>
+                                        <option value="">Feito</option>
+                                    </select>
+                                    <button type="button" className="tarefa">alterar status</button>
+                                </section>
+                            </ul>
+                        </section>
+                    ))}
 
-                    <section className="cartao">
-                        <ul className="tarefa">
-                            <section>
-                                <li>Descrição: </li>
-                                <li>Setor: </li>
-                                <li>Prioridade: </li>
-                                <li>Vinculado a: </li>
-                            </section>
-                            <section>
-                                <button className="tarefa">editar</button>
-                                <button className="tarefa">excluir</button>
-                            </section>
-                            <section>
-                                <select name="nome" id="id">
-                                    <option value="">A fazer</option>
-                                    <option value="">Fazendo</option>
-                                    <option value="">Feito</option>
-                                </select>
-                                <button type="button" className="tarefa">alterar status</button>
-                            </section>
-                        </ul>
-                    </section>
 
                 </section>
                 <section className="coluna">
